634874d9d81ecb8d211838d6d8df1c2522d80b8a
Benjamin Winward <benjamin.w627@stu.nebo.edu>
2021-11-03 13:45:37 -0600

    lif

--- FPS Prototype/Assets/Scripts/Enemy.cs
+++ FPS Prototype/Assets/Scripts/Enemy.cs
@@ -38,7 +38,7 @@ public class Enemy : MonoBehaviour
         NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
 
         //Save path as a list
-        path = navMesh.corners.ToList();
+        path = navMeshPath.corners.ToList();
     }
 
     void ChaseTarget()
@@ -48,17 +48,33 @@ public class Enemy : MonoBehaviour
         //Move towards the player
         transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3(0, yPathOffset, 0), moveSpeed * Time.deltaTime);
 
-        if(transform.positon == path[0] + new Vector3(0, yPathOffset, 0))
+        if(transform.position == path[0] + new Vector3(0, yPathOffset, 0))
             path.RemoveAt(0);
 
+    }
+    public void TakeDamage(int damage)
+    {
+        curHP -= damage;
+        if(curHP <= 0)
+            Die();
+    }
+    void Die()
+    {
+        Destroy(gameObject);
     }
     // Update is called once per frame
     void Update()
     {
+        //Look at Target
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = Vector3.up * angle;
+        
         float dist = Vector3.Distance(transform.position, target.transform.position);
 
         if(dist <= attackRange){
-            if(weapon.CanShoot)
+            if(weapon.CanShoot())
                 weapon.Shoot();
         }
         else
